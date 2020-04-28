e0069ac99391c55ce9eb053893369a3f9f4fed96
Dominic Gaiero <dpgaiero@gmail.com>
2020-04-27 22:30:00 -0700

    feat: add title to 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,16 @@
 import { Result, Button } from 'antd';
 import Router from 'next/router'
+import Title from '../components/Title'
 
 export default function ErrorPage() {
    return (
+      <>
+         <title><Title name="Page not Found"></Title></title>
    <Result
       status="404"
       title="404"
       subTitle="Sorry, the page you visited does not exist."
       extra={<Button type="primary" onClick={() => Router.push('/')}><a>Back Home</a></Button>}
    />
+   </>
    )};
