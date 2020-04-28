8047920bf3ac09d8b1d843a5dd518ea15eba94cd
Dominic Gaiero <dpgaiero@gmail.com>
2020-04-27 22:26:00 -0700

    feat: add favicons

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,16 @@ export default class MyDocument extends Document {
       return (
          <html>
             <Head>
+               <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+               <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+               <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+               <link rel="manifest" href="/site.webmanifest" />
+               <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#aa7e02" />
+               <meta name="msapplication-TileColor" content="#1e1a17" />
+               <meta name="theme-color" content="#aa7e02" />
+
                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+               <link rel="stylesheet" href="https://use.typekit.net/ouw0vbj.css" />
             </Head>
             <body>
                <Main />
