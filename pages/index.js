f7232b08eaa16148aa1fe0ad33cef07e9c6c4bb6
Dominic Gaiero <dpgaiero@gmail.com>
2020-06-21 15:46:47 -0600

    chore: update imports and clean up code

--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,50 @@
 import '../assets/less/index/main.less'
 
-import { Affix, Col, Layout, Menu, Row } from 'antd';
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
 
-import Breadcrumb from '../components/Breadcrumb'
 import Footer from '../components/Footer'
-import Head from 'next/head'
+import Head from '../components/Head'
 import Header from '../components/Header'
-import { HiThere }  from '../public/static/images/index/hi-there.svg'
-import Link from 'next/link'
-import Logo from '../public/static/images/logos/logos_name-dark.svg'
-import ParticleConfig from '../assets/json/index/particlesjs-config.json'
-import Particles from 'react-particles-js';
-import Title from '../components/Title'
-import { withRouter } from 'next/router'
+import { NBSP } from '../components/helper';
+import UA from '../components/UnderlineA'
 
-const { SubMenu } = Menu;
+// import Head from 'next/head'
+
+
+
+
+
 const { Content } = Layout;
 
-const hiThereStyle = {
-   height: '9em',
-}
-
-function Index() {
+export default function() {
    return (
       <>
-         <Head>
+      <Head title="Home"/>
+         {/* <Head>
+            <title>Test</title>
+            <Title name="Home"/>
             <title><Title name="Home"></Title></title>
-            {/* <title>Test</title> */}
-         </Head>
+         </Head> */}
          <Layout style={{ height: '100%' }}>
             <Header />
             <Content className="home-page-container">
-               {/* <Breadcrumb style={{ margin: '16px 0' }} /> */}
-               <div className="site-layout-content-header-wrapper">
-                  <div className="site-layout-content-header">
-                     <div className="header-image-picture-wrapper">
-                        {/* <Col> */}
-                           <figure className="headshot-image-frame"><img className="headshot-image" src="/static/images/headshot.webp" /></figure>
-                           {/* <figure className="headshot-image-frame"><img className="headshot-image" src="https://picsum.photos/1088/933.jpg" /></figure> */}
-                           <span className="hello-index">hi there.</span>
-                           {/* <HiThere className="hi-there"/> */}
-                        {/* </Col>
-                        <Col>
-                        </Col> */}
+               <div className="welcome-card-wrapper">
+                  <div className="welcome-card">
+                     <div className="welcome-card-headshot-wrapper">
+                           <figure className="welcome-card-headshot">
+                              <img
+                                 src="/static/images/headshot.webp"
+                              />
+                           </figure>
+                           <span className="hi-there-text">hi there.</span>
                      </div>
                      <div style={{ marginTop: 16 }}>
-                        {/* <div className="headshot-image-frame"><img className="headshot-image" src="https://picsum.photos/1088/933.jpg" /></div> */}
-                        
-                        <p className="welcome-text">
-                           I'm Dominic, currently attending <a className="underline-link" href="https://calpoly.edu" target="_blank" rel="noopener noreferrer">Cal Poly San Luis
-                           Obispo</a> studying computer engineering. Some random text here. I'm gonna put a lot of random text in this area so that it grows to multiple lines. Some random text here. I'm gonna put a lot of random text in this area so that it grows to multiple lines. Some random text here. I'm gonna put a lot of random text in this area so that it grows to multiple lines. Some random text here. I'm gonna put a lot of random text in this area so that it grows to multiple lines.
-                              </p>
+                        <p>
+                           I'm Dominic, currently attending <UA href="https://calpoly.edu" text="Cal Poly San Luis Obisp" _blank />{NBSP}
+                           studying computer engineering. Some random text here. I'm gonna put a lot of random text in this area so that it grows to multiple lines. Some random text here. I'm gonna put a lot of random text in this area so that it grows to multiple lines. Some random text here. I'm gonna put a lot of random text in this area so that it grows to multiple lines. Some random text here. I'm gonna put a lot of random text in this area so that it grows to multiple lines.
+                        </p>
                      </div>
                   </div>
-                  {/* <Particles className='particles' params={ParticleConfig}/> */}
                </div>
             </Content>
             <Footer />
@@ -62,5 +52,3 @@ function Index() {
       </>
    );
 };
-
-export default Index;
