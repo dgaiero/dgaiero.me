847230cad4f4e31078a7d00d1aa5749e36e26822
Dominic Gaiero <dpgaiero@gmail.com>
2020-08-09 17:06:07 -0600

    chore(next.js): update config file

--- next.config.js
+++ next.config.js
@@ -1,60 +1,138 @@
-require('dotenv').config();
-const lessToJS = require('less-vars-to-js')
-const fs = require('fs')
-const path = require('path');
-// const withSass = require('@zeit/next-sass');
-const withLess = require('@zeit/next-less');
-const withCSS = require('@zeit/next-css');
-const withPlugins = require('next-compose-plugins');
+https://github.com/timothymiller/mern-ant-design-graphql-next-template/blob/master/next.config.js
 
-// Where your antd-custom.less file lives
-const themeVariables = lessToJS(fs.readFileSync(path.resolve(__dirname, './assets/less/antd-custom.less'), 'utf8'));
+// Serve images in WebP where available & at reduced size when possible.
+const withOptimizedImages = require('next-optimized-images')
+
+// Service Worker for Progressive Web App (PWA) standards
+const withOffline = require('next-offline')
+
+// .env Environment variables
+require("dotenv").config();
+const webpack = require("webpack");
 
 const nextConfig = {
    env: {
       SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
       CONTACT_EMAIL_FROM_ADDRESS: process.env.CONTACT_EMAIL_FROM_ADDRESS,
    },
-   distDir: '.next',
 };
 
-const plugins = [
-   withCSS({
-      cssModules: true
-   }),
-   withLess({
-      lessLoaderOptions: {
-         javascriptEnabled: true,
-         modifyVars: themeVariables,
-      },
-      webpack: (config, { isServer }) => {
-         if (isServer) {
-            const antStyles = /antd\/.*?\/style.*?/;
-            const origExternals = [...config.externals];
-            config.externals = [
-               (context, request, callback) => {
-                  if (request.match(antStyles)) return callback();
-                  if (typeof origExternals[0] === 'function') {
-                     origExternals[0](context, request, callback);
-                  } else {
-                     callback();
-                  }
-               },
-               ...(typeof origExternals[0] === 'function' ? [] : origExternals),
-            ];
+// Bundle Analyzer
+const withBundleAnalyzer = require("@zeit/next-bundle-analyzer")
 
-            config.module.rules.unshift({
-               test: antStyles,
-               use: 'null-loader',
+// .CSS support
+const withCss = require('@zeit/next-css')
+
+// .LESS support
+/* eslint-disable */
+const withLess = require('@zeit/next-less')
+const lessToJS = require('less-vars-to-js')
+const fs = require('fs')
+const path = require('path')
+
+// Where your antd-custom.less file lives
+const themeVariables = lessToJS(fs.readFileSync(path.resolve(__dirname, './assets/less/antd-custom.less'), 'utf8'))
+
+module.exports = withBundleAnalyzer(withOffline(withOptimizedImages(withCss(withLess({
+   // Now by ZEIT deployment target
+   target: 'serverless',
+   // Service Worker for Progressive Web App (PWA) standards
+   transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
+   // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
+   // turn on the SW in dev mode so that we can actually test it
+   generateInDevMode: true,
+   workboxOpts: {
+      swDest: 'static/service-worker.js',
+      runtimeCaching: [
+         {
+            urlPattern: /^https?.*/,
+            handler: 'NetworkFirst',
+            options: {
+               cacheName: 'https-calls',
+               networkTimeoutSeconds: 15,
+               expiration: {
+                  maxEntries: 150,
+                  maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+               },
+               cacheableResponse: {
+                  statuses: [0, 200],
+               },
             },
-            {
-               test: /\.(png|jp(e*)g|svg|gif)$/,
-               use: ['@svgr/webpack']
-            });
-         }
-         return config;
+         },
+      ],
+   },
+   // Deliver images in the smallest size (in increments of 200).
+   sizes: [200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000],
+   // Webpack Bundle Analyzer
+   analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+   analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+   bundleAnalyzerConfig: {
+      server: {
+         analyzerMode: 'static',
+         reportFilename: '../bundles/server.html'
       },
-   }),
-   // withSass,
-];
-module.exports = withPlugins(plugins, nextConfig);
+      browser: {
+         analyzerMode: 'static',
+         reportFilename: '../bundles/client.html'
+      }
+   },
+   // Markdown in JSX filetype support
+   pageExtensions: ['mdx', 'md', 'jsx', 'js'],
+   // Locally scored CSS modules
+   // cssModules: true,
+   cssLoaderOptions: {
+      importLoaders: 1,
+      localIdentName: "[local]___[hash:base64:5]",
+   },
+   // custom webpack config for Ant Design Less
+   lessLoaderOptions: {
+      javascriptEnabled: true,
+      modifyVars: themeVariables, // make your antd custom effective
+   },
+   webpack: (config, { isServer }) => {
+      if (isServer) {
+         const antStyles = /antd\/.*?\/style.*?/
+         const origExternals = [...config.externals]
+         config.externals = [
+            (context, request, callback) => {
+               if (request.match(antStyles)) return callback()
+               if (typeof origExternals[0] === 'function') {
+                  origExternals[0](context, request, callback)
+               } else {
+                  callback()
+               }
+            },
+            ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+         ]
+
+         config.module.rules.unshift({
+            test: antStyles,
+            use: 'null-loader',
+         })
+      }
+
+      // Fixes npm packages that depend on `fs` module
+      config.node = {
+         fs: 'empty'
+      }
+      /**
+       * Returns environment variables as an object
+       */
+      const env = Object.keys(process.env).reduce((acc, curr) => {
+         acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+         return acc;
+      }, {});
+
+      /** Allows you to create global constants which can be configured
+      * at compile time, which in our case is our environment variables
+      */
+      config.plugins.push(new webpack.DefinePlugin(env));
+
+      config.module.rules.push({
+         test: /\.md$/,
+         use: 'raw-loader',
+      });
+
+      return config
+   }
+})))), nextConfig);
